136e1d157bbba88d69832993ace237a7b4211a02
NoinBlake <josechocobar336@gmail.com>
2023-06-05 21:13:05 -0300

    fecth

--- public/index.js
+++ public/index.js
@@ -26,103 +26,103 @@ let listaPalabras = [
         opcion3: "Ulan Bator",
         opcion4: "AnguilaThe Valley",
         respuestaCorrecta: "Ulan Bator"
-    },
-    pregunta4 = {
-        texto: "¿Cómo se denomina el resultado de la multiplicación?",
-        opcion1: "producto",
-        opcion2: "sustracción",
-        opcion3: "reactivo",
-        opcion4: "adición",
-        respuestaCorrecta: "producto"
-    },
-    pregunta5 = {
-        texto: "¿Cual es el oceano mas grande del mundo?",
-        opcion1: "Atlantico",
-        opcion2: "Indico",
-        opcion3: "Pacifico",
-        opcion4: "Artico",
-        respuestaCorrecta: "Pacifico"
-    },
-    pregunta6 = {
-        texto: "En que año llego Cristobal Colon a America?",
-        opcion1: "1492",
-        opcion2: "1501",
-        opcion3: "1490",
-        opcion4: "1600",
-        respuestaCorrecta: "1492"
-    },
-    pregunta7 = {
-        texto: "¿Quien es el padre del psicoanalisis?",
-        opcion1: "William James",
-        opcion2: "Hippolyte Bernheim",
-        opcion3: "Sigmund Freud",
-        opcion4: "Jean Piaget",
-        respuestaCorrecta: "Sigmund Freud"
-    },
-    pregunta8 = {
-        texto: "¿Que estudia la cartografia?",
-        opcion1: "Estudia la produccion de cartones",
-        opcion2: "Estudia las estrellas",
-        opcion3: "Estudia la escritura de cartas",
-        opcion4: "Estudia los mapas",
-        respuestaCorrecta: "Estudia los mapas"
-    },
-    pregunta9 = {
-        texto: "¿Cual es el pais mas grande del mundo?",
-        opcion1: "Rusia",
-        opcion2: "Brasil",
-        opcion3: "China",
-        opcion4: "Estados Unidos",
-        respuestaCorrecta: "Rusia"
-    },
-    pregunta10 = {
-        texto: "¿En Donde esta ubicada la torre Eiffel?",
-        opcion1: "Francia",
-        opcion2: "España",
-        opcion3: "Dinamarca",
-        opcion4: "Italia",
-        respuestaCorrecta: "Francia"
-    },
-    pregunta11 = {
-        texto: "¿Que deporte practicaba Michael Jordan?",
-        opcion1: "Rugby",
-        opcion2: "Boxeo",
-        opcion3: "Futbol",
-        opcion4: "Basket",
-        respuestaCorrecta: "Basket"
-    },
-    pregunta12 = {
-        texto: "¿En que año comenzo la II guerra mundial?",
-        opcion1: "1943",
-        opcion2: "1939",
-        opcion3: "1941",
-        opcion4: "1993",
-        respuestaCorrecta: "1939"
-    },
-    pregunta13 = {
-        texto: "¿Cual es el pais con mas poblacion de la tierra?",
-        opcion1: "Estados Unidos",
-        opcion2: "Rusia",
-        opcion3: "China",
-        opcion4: "Brasil",
-        respuestaCorrecta: "China"
-    },
-    pregunta14 = {
-        texto: "¿Quien fue el general de los nazis en la segunda guerra mundial?",
-        opcion1: "Bin Laden",
-        opcion2: "Adolf Hitler",
-        opcion3: "Napoleon",
-        opcion4: "John F Kennedy",
-        respuestaCorrecta: "Adolf Hitler"
-    },
-    pregunta15 = {
-        texto: "¿Cual es el primer numerp primo?",
-        opcion1: "5",
-        opcion2: "11",
-        opcion3: "7",
-        opcion4: "2",
-        respuestaCorrecta: "2",
     }
+    // pregunta4 = {
+    //     texto: "¿Cómo se denomina el resultado de la multiplicación?",
+    //     opcion1: "producto",
+    //     opcion2: "sustracción",
+    //     opcion3: "reactivo",
+    //     opcion4: "adición",
+    //     respuestaCorrecta: "producto"
+    // },
+    // pregunta5 = {
+    //     texto: "¿Cual es el oceano mas grande del mundo?",
+    //     opcion1: "Atlantico",
+    //     opcion2: "Indico",
+    //     opcion3: "Pacifico",
+    //     opcion4: "Artico",
+    //     respuestaCorrecta: "Pacifico"
+    // },
+    // pregunta6 = {
+    //     texto: "En que año llego Cristobal Colon a America?",
+    //     opcion1: "1492",
+    //     opcion2: "1501",
+    //     opcion3: "1490",
+    //     opcion4: "1600",
+    //     respuestaCorrecta: "1492"
+    // },
+    // pregunta7 = {
+    //     texto: "¿Quien es el padre del psicoanalisis?",
+    //     opcion1: "William James",
+    //     opcion2: "Hippolyte Bernheim",
+    //     opcion3: "Sigmund Freud",
+    //     opcion4: "Jean Piaget",
+    //     respuestaCorrecta: "Sigmund Freud"
+    // },
+    // pregunta8 = {
+    //     texto: "¿Que estudia la cartografia?",
+    //     opcion1: "Estudia la produccion de cartones",
+    //     opcion2: "Estudia las estrellas",
+    //     opcion3: "Estudia la escritura de cartas",
+    //     opcion4: "Estudia los mapas",
+    //     respuestaCorrecta: "Estudia los mapas"
+    // },
+    // pregunta9 = {
+    //     texto: "¿Cual es el pais mas grande del mundo?",
+    //     opcion1: "Rusia",
+    //     opcion2: "Brasil",
+    //     opcion3: "China",
+    //     opcion4: "Estados Unidos",
+    //     respuestaCorrecta: "Rusia"
+    // },
+    // pregunta10 = {
+    //     texto: "¿En Donde esta ubicada la torre Eiffel?",
+    //     opcion1: "Francia",
+    //     opcion2: "España",
+    //     opcion3: "Dinamarca",
+    //     opcion4: "Italia",
+    //     respuestaCorrecta: "Francia"
+    // },
+    // pregunta11 = {
+    //     texto: "¿Que deporte practicaba Michael Jordan?",
+    //     opcion1: "Rugby",
+    //     opcion2: "Boxeo",
+    //     opcion3: "Futbol",
+    //     opcion4: "Basket",
+    //     respuestaCorrecta: "Basket"
+    // },
+    // pregunta12 = {
+    //     texto: "¿En que año comenzo la II guerra mundial?",
+    //     opcion1: "1943",
+    //     opcion2: "1939",
+    //     opcion3: "1941",
+    //     opcion4: "1993",
+    //     respuestaCorrecta: "1939"
+    // },
+    // pregunta13 = {
+    //     texto: "¿Cual es el pais con mas poblacion de la tierra?",
+    //     opcion1: "Estados Unidos",
+    //     opcion2: "Rusia",
+    //     opcion3: "China",
+    //     opcion4: "Brasil",
+    //     respuestaCorrecta: "China"
+    // },
+    // pregunta14 = {
+    //     texto: "¿Quien fue el general de los nazis en la segunda guerra mundial?",
+    //     opcion1: "Bin Laden",
+    //     opcion2: "Adolf Hitler",
+    //     opcion3: "Napoleon",
+    //     opcion4: "John F Kennedy",
+    //     respuestaCorrecta: "Adolf Hitler"
+    // },
+    // pregunta15 = {
+    //     texto: "¿Cual es el primer numerp primo?",
+    //     opcion1: "5",
+    //     opcion2: "11",
+    //     opcion3: "7",
+    //     opcion4: "2",
+    //     respuestaCorrecta: "2",
+    // }
 ];
 
 const buttonPlay = document.querySelector("#buttonPlay");
@@ -189,14 +189,14 @@ const playGame=async()=>{
     } catch (error) {
         console.error("El error es", error);
     };
-    return preguntas(number);
+    preguntas(number);
 };
 
 // PASO 2: Traer preguntas del array;
 const preguntas=(random)=> {
 
     if (listaPalabras.length === 0) {
-        sendData(playerName,correctPoints,URL);
+        sendData(playerName,correctPoints);
         openHighscore();
     } else {
         htmlQuestion.innerText = listaPalabras[random].texto;
@@ -318,13 +318,15 @@ const sendData = async (name, points) => {
     });
 };
 
-const getData = async () => {
-    const res = await fetch(`${URL}scoreboard`);
-    const data = await res.json();
-    
-    data.map(items=>{
-        renderHighscore(items);
-    });
+const getData =() => {
+    setTimeout(async()=>{
+        const res = await fetch(`${URL}scoreboard`);
+        const data = await res.json();
+        
+        data.map(items=>{
+            renderHighscore(items);
+        });
+    },1000)
 };
 
 
